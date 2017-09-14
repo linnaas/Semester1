d0e7dd46aecc01e0a50e4fad97145bd8f0933bb2
Petter Hanssen <petterhanssen96@gmail.com>
2017-09-14 20:10:08 +0200

    Nye kommentarer

--- PRG1000/eksempler/tema3/6.php
+++ PRG1000/eksempler/tema3/6.php
@@ -10,10 +10,11 @@ Skriv ut tall fra 1 til:    <input type="text" name="tall">
 </body>
 </html>
 <?php
-if (isset($_POST["submit"])){ // "isset" funksjonen brukes for å sjekke om "submit" knappen er trykket.
-  $svar=$_POST["tall"];
-  for ($i=1; $i <= $svar; $i++) {
-      print("$i<br>");
+if (isset($_POST["submit"])){ // "isset" funksjonen brukes for å sjekke om "submit" knappen er trykket. Kan også droppe denne koden og ha php koden i en egen fil som vi har brukt
+  $svar=$_POST["tall"]; // Henter tallet brukeren har skrevet i tekstfeltet og lagrer det i variablen $svar
+  for ($i=tall; $tall <= $svar; $tall++) { // Så lenge tallet som blir printet ut er mindre eller likt tallet i tekstfeltet så skrives det ut til brukeren og kjører koden på nytt.
+      print("$tall<br>"); // Printer ut tallet til brukeren av nettsiden.
   }
 }
 ?>
+
