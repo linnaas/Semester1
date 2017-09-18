c64b175c275074b411b99be7bdbc4748d4844531
Petter Hanssen <petterhanssen96@gmail.com>
2017-09-18 12:51:01 +0200

    fikset noen kommentarer

--- PRG1000/eksempler/tema4/1.php
+++ PRG1000/eksempler/tema4/1.php
@@ -19,8 +19,8 @@ $fornavn=$_POST["fornavn"];
 $etternavn=$_POST["etternavn"];
 $navn=$fornavn." ".$etternavn. "\n";
 
-$tekstfil = fopen("navn.txt", "a+"); // Åpne tekstfilen, "a+" (Read/Write. Preserves file content by writing to the end of the file)
-fwrite($tekstfil, $navn); // Skriver variablen $student med linjeskift. ("\n")
+$tekstfil = fopen("navn.txt", "a+"); // Åpne tekstfilen, "a+" = (Read/Write. Preserves file content by writing to the end of the file)
+fwrite($tekstfil, $navn); // Skriver variablen $navn. Denne variablen er "lagret" med linjeskift på slutten. ("\n")
 fclose($tekstfil); // Lukk filen
 print("$navn er nå registrert.");
 
