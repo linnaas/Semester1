d21b4c50c2378cef3caad650b8ff0f77c6ff9bf1
Petter Hanssen <petterhanssen96@gmail.com>
2017-09-21 16:42:43 +0200

    oppgave 1

---
+++ PRG1000/oppgave1/README.md
@@ -0,0 +1,2 @@
+# PRG1000-Oppgave1
+Obligatorisk oppgave 1 PRG1000.
---
+++ PRG1000/oppgave1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta content="ie=edge" http-equiv="X-UA-Compatible">
+	<title>Hjem</title>
+</head>
+<body>
+	  <h1>Obligatorisk oppgave 1</h1>
+  	<h2>Brukerfunksjoner</h2>
+  <ul>
+    <li><a href="registrerklasse.php">Registrer klasse</a></li>
+    <li><a href="visklasser.php">Vis alle klasser</a></li>
+
+    <li><a href="registrerstudent.php">Registrer student</a></li>
+    <li><a href="visstudenter.php">Vis alle studenter</a></li>
+    <li><a href="velgklasse.php">Vis klasselsite</a></li>
+  </ul>
+  <h2>Tekstfiler</h2>
+  <ul>
+    <li><a href="klasse.txt">klasse.txt<a/></li>
+    <li><a href="student.txt">student.txt<a/></li>
+  </ul>
+</body>
+</html>
